d4469d416899d10d9872da579049fca8aa5705a5
Efemiaya Favour Oghenetega <EfemiayaFavour@gmail.com>
2023-12-05 22:06:23 +0100

    feat: Implement dynamic grid resizing and color changes
    - Added event listeners for hover effects on grid elements
    - Randomized background color on mouse leave
    - Implemented grid resizing functionality with user input
    - Created a reset button to prompt for a new grid size

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ function createGrid(gridSize) {
     for (let j = 0; j < gridSize; j++) {
       const cell = document.createElement('div');
       cell.classList.add('grid-cell');
-      cell.textContent = j;
+      addElementEventListeners(cell);
       row.append(cell);
     }
   }
@@ -18,6 +18,49 @@ function createGrid(gridSize) {
   return gridContainer;
 }
 
-const newGridContainer = createGrid(16);
+function addElementEventListeners(element) {
+  element.addEventListener("mouseenter", () => {
+    element.style.backgroundColor = "green";
+  });
+
+  element.addEventListener("mouseleave", () => {
+    element.style.backgroundColor = getRandomColor();
+  });
+}
+
+function getRandomColor() {
+  const r = Math.floor(Math.random() * 256);
+  const g = Math.floor(Math.random() * 256);
+  const b = Math.floor(Math.random() * 256);
+  return `rgb(${r}, ${g}, ${b})`;
+}
+
+function getSize() {
+  let newSize = prompt("Enter the number of squares per side (up to 100):");
+
+  if (newSize === null || isNaN(newSize) || newSize < 1 || newSize > 150) {
+    alert("Invalid input. Please enter a number between 1 and 150.");
+    return;
+  }
+
+  newSize = parseInt(newSize);
+
+  // Call a function to reset the grid with the new size
+  resetGridWithSize(newSize);
+}
+
+function resetGridWithSize(size) {
+  const newGridContainer = createGrid(size);
+  // Replace the old grid with the new one
+  const container = document.querySelector('.container');
+  container.textContent = ''; // Clear the container
+  container.append(newGridContainer);
+}
+
+const resetButton = document.querySelector("#resetButton");
+resetButton.addEventListener('click', getSize);
+
+
+const newGridContainer = createGrid(150);
 container = document.querySelector('.container');
-container.append(newGridContainer); // Append the new grid container to the body
+container.append(newGridContainer);
